4edcba969b3b680e21fce8f6acb7c978817afe4b
Schubert Anselme <schubert@anselm.es>
2025-10-22 15:14:24 -0400

    Add Message type and CloudEventOptions struct definitions
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ cloudevent/message.go
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
+// Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
+
+type Message map[string]string
---
+++ cloudevent/option.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+// Copyright (c) 2025 Schubert Anselme <schubert@anselm.es>
+
+type CloudEventOptions struct {
+  Source string
+  Type   string
+}
